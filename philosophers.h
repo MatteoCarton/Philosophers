7faab838d2e0aa1b27eeb3613a51aae0ab6e7765
Mattéo <mcarton@student.s19.be>
2025-04-12 22:04:10 +0200

    init

---
+++ philosophers.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarton <mcarton@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 21:20:32 by mcarton           #+#    #+#             */
+/*   Updated: 2025/04/12 22:02:28 by mcarton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
+
+#include <pthread.h>
+
+typedef enum {
+    false,
+    true
+} bool;
+
+typedef enum {
+    eating,
+    thinking,
+    sleeping
+}   philo_state;
+
+typedef struct s_fork {
+    pthread_mutex_t mutex;
+    bool is_taken;
+} t_fork;
+
+typedef struct s_rules {
+    int number_of_philosophers;
+    long long time_to_die;
+    long long time_to_eat;
+    long long time_to_sleep;
+    int number_of_times_each_philosopher_must_eat;
+} t_rules;
+
+typedef struct s_philosopher
+{
+    pthread_t philo_thread;
+    t_rules *rules;
+    int philo_id;
+    philo_state current;
+    t_fork right_fork;
+    t_fork left_fork;
+    long long last_meal_time;
+    
+} t_philosopher;
+
+
+#endif
